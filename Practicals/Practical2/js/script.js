a1048304cfaa7cc57bb342fc1d1084b365c725b0
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-02-02 14:08:17 +0530

    made some changes in styling.

--- Practicals/Practical2/js/script.js
+++ Practicals/Practical2/js/script.js
@@ -1,8 +1,6 @@
-
 function myMap() {
-  // The location of simform solutions office
-  const ss_office = { lat: 23.028217245452694, lng: 72.49937209773258 }; // Set the zoom of the map
-  var mapOptions = { zoom: 17.56, center: ss_office }; // Create the map, centered at ss_office
+  const ss_office = { lat: 23.028217245452694, lng: 72.49937209773258 };
+  var mapOptions = { zoom: 17.56, center: ss_office };
   const googlemap = new google.maps.Map(
     document.getElementById("googleMap"),
     mapOptions
@@ -12,11 +10,9 @@ function myMap() {
     map: googlemap,
   });
 }
-const cursor = document.querySelector('.cursorPointer');
+const cursor = document.querySelector(".cursorPointer");
 
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-    
-
-})
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+});
