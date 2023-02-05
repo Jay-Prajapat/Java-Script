9b9b3cadb332b0047b7d38196464a76d03dee9aa
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-02-05 23:45:43 +0530

    Search Functionality implemented.

--- Practicals/Practical3/js/script.js
+++ Practicals/Practical3/js/script.js
@@ -1,72 +1,103 @@
 function onSubmit() {
+
   let prodData = JSON.parse(localStorage.getItem("prodData")) || [];
 
-  if (validateID(prodData)) {
-   
-      const fileEl = document.getElementById("image");
+  if (validateData()) {
 
-      const fr = new FileReader();
-      fr.readAsDataURL(fileEl.files[0]);
+    const fileEl = document.getElementById("image");
 
-      fr.addEventListener("load", () => {
-        prodData.push({
-          prodId: document.getElementById("prodId").value,
-          name: document.getElementById("name").value,
-          image: fr.result,
-          price: document.getElementById("price").value,
-          description: document.getElementById("description").value,
-        });
+    const fr = new FileReader();
+    fr.readAsDataURL(fileEl.files[0]);
 
-        localStorage.setItem("prodData", JSON.stringify(prodData));
+    fr.addEventListener("load", () => {
+      prodData.push({
+        prodId: document.getElementById("prodId").value,
+        name: document.getElementById("name").value,
+        image: fr.result,
+        price: document.getElementById("price").value,
+        description: document.getElementById("description").value,
       });
-    
+
+      localStorage.setItem("prodData", JSON.stringify(prodData));
+    });
+
   } else {
     console.log("no id err1");
     error.textContent = "Please enter a product id.";
   }
 }
 
-function validateData(data) {
-  data.forEach((item) => {
-    if (item.name == "") {
-      console.log("err1");
-      error.textContent = "Please enter a product name.";
-    } else if (item.price == "") {
-      console.log("err1");
-      error.textContent += "Please enter price of product.";
-    } else if (item.description == "") {
-      console.log("err1");
-      error.textContent = "Please enter description of product.";
-    } else if (item.image == undefined) {
-      console.log("err1");
-      error.textContent = "Please choose product image.";
-    } else {
-      console.log("no err1");
-      error.textContent = "";
-    }
-  });
+function validID() {
+  
 }
 
+function validateData() {
+  let name = document.getElementById('name').value;
+  let price = document.getElementById('price').value;
+  let id = document.getElementById('prodId').value;
+
+  if (id == "") {
+    document.getElementById('pId').style.display = 'block';
+    document.getElementById('pId').innerHTML = "Product Id is required !!";
+    return false;
+  }
+  if(validID(id)){
+
+  }
+}
+
+
+
+function searchById() {
+  let id = document.getElementById('search');
+  let prodData = JSON.parse(localStorage.getItem("prodData")) || [];
+  let html = ``;
+  prodData.forEach((element, index) => {
+    if (element.prodId == id.value || element.name.toLowerCase().includes(id.value.toLowerCase())) {
+      console.log(element.prodId);
+      html += `
+        <div class="column">
+          <div class="card">
+            <h3>${element.name}</h3>
+            <img src="${element.image}" alt="image">
+            <p class="id">${element.prodId}</p>
+            <p class="price">&#8377 ${element.price}</p>
+            <p class="desc">${element.description}</p>
+            <button type="button" onclick="deleteData(${index})" class="btn btn-danger">
+          Delete
+        </button>
+        <button type="button" onclick="edit(${index})" class="btn btn-info">
+          Edit
+        </button>
+          </div>
+        </div>
+        `;
+      document.querySelector("#insertData").innerHTML = html;
+
+    }
+  });
+  id.value = '';
+}
 
 function sortByNameAsc() {
   let prodData = JSON.parse(localStorage.getItem("prodData"));
 
   document.getElementById('sortByName').innerHTML = `<button onclick="sortByNameDes()"><i class="fa fa-arrow-up"></i> Product name</button>`;
 
-  prodData.sort((a,b) => {
+  prodData.sort((a, b) => {
     let str1 = a.name.toLowerCase();
     let str2 = b.name.toLowerCase();
 
-    if(str1 < str2){
+    if (str1 < str2) {
       return -1;
     }
-    if(str1 > str2){
+    if (str1 > str2) {
       return 1;
     }
     return 0;
   })
 
-  localStorage.setItem('prodData',JSON.stringify(prodData));
+  localStorage.setItem('prodData', JSON.stringify(prodData));
   showData();
 }
 
@@ -75,20 +106,20 @@ function sortByNameDes() {
 
   document.getElementById('sortByName').innerHTML = `<button onclick="sortByNameAsc()"><i class="fa">&#xf063;</i> Product name</button>`;
 
-  prodData.sort((a,b) => {
+  prodData.sort((a, b) => {
     let str1 = a.name.toLowerCase();
     let str2 = b.name.toLowerCase();
 
-    if(str1 < str2){
+    if (str1 < str2) {
       return 1;
     }
-    if(str1 > str2){
+    if (str1 > str2) {
       return -1;
     }
     return 0;
   })
 
-  localStorage.setItem('prodData',JSON.stringify(prodData));
+  localStorage.setItem('prodData', JSON.stringify(prodData));
   showData();
 }
 
@@ -97,13 +128,13 @@ function sortByPriceAsc() {
 
   document.getElementById('sortByprice').innerHTML = `<button onclick="sortByPriceDes()"><i class="fa fa-arrow-up"></i> Product Price</button>`;
 
-  prodData.sort((a,b) => {
+  prodData.sort((a, b) => {
     let str1 = a.price;
     let str2 = b.price;
     return str1 - str2;
   })
 
-  localStorage.setItem('prodData',JSON.stringify(prodData));
+  localStorage.setItem('prodData', JSON.stringify(prodData));
   showData();
 }
 
@@ -112,13 +143,13 @@ function sortByPriceDes() {
 
   document.getElementById('sortByprice').innerHTML = `<button onclick="sortByPriceAsc()"><i class="fa">&#xf063;</i> Product Price</button>`;
 
-  prodData.sort((a,b) => {
+  prodData.sort((a, b) => {
     let str1 = a.price;
     let str2 = b.price;
     return str2 - str1;
   })
 
-  localStorage.setItem('prodData',JSON.stringify(prodData));
+  localStorage.setItem('prodData', JSON.stringify(prodData));
   showData();
 }
 
